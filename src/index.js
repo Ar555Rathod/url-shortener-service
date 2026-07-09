659ea5c58b0f02d72c2cd566de40b0eb19f4bcbc
Ar555Rathod <82928218+Ar555Rathod@users.noreply.github.com>
2026-07-09 22:52:29 +0530

    chore: add basic express app

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => res.send({status: 'ok'}));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on ${port}`));
